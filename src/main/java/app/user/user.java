86828a23fb5db28695192827670579c7e19001fa
balkahun <balka@vipmail.hu>
2016-08-06 18:59:08 +0200

    fixed compile errors

--- src/main/java/app/user/user.java
+++ src/main/java/app/user/user.java
@@ -2,7 +2,43 @@ package app.user;
 
 import lombok.*;
 @Value public class user {
-	String username;
-	    String salt;
-	    String hashedPassword;
+
+	    
+	   private String username;
+	private String salt;
+	private String hashedPassword;
+
+	public user(String username, String salt, String hashedPassword) {
+		this.username = username;
+		this.salt = salt;
+		this.hashedPassword = hashedPassword;
+		}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+	public String getHashedPassword() {
+		return hashedPassword;
+	}
+
+	public void setHashedPassword(String hashedPassword) {
+		this.hashedPassword = hashedPassword;
+	}
+
+	public String toString(){
+		   return "true";
+	   }
 }
